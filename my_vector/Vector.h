fddeac904dd74395aa20aa6b6a7e37307775b3d6
UgniusPi <upilipavicius@gmail.com>
2026-05-29 12:20:13 +0300

    update

--- my_vector/Vector.h
+++ my_vector/Vector.h
@@ -7,5 +7,14 @@ namespace mystl {
 
 template<typename T>
 class Vector {
+public:
+    using value_type = T;
+    using size_type = std::size_t;
+    using reference = T&;
+    using const_reference = const T&;
+    using pointer = T*;
+    using const_pointer = const T*;
+    using iterator = T*;
+    using const_iterator = const T*;
 };
 } // namespace mystl
